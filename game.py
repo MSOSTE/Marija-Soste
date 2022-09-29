d4610ed83aef1968371c9cc5d7747874f4964bda
msoste <marijasoste@gmail.com>
2022-09-29 12:33:38 +0300

    game.py,  slicer.py

---
+++ game.py
@@ -0,0 +1,18 @@
+def shoot()
+
+
+def game_over()
+    print('Game over Marija')
+
+def fly_left()
+    print('You moved left')
+
+def fly_right()
+    print('You moved right')
+
+def fly_forward()
+    print('You moved forward')
+
+
+if __name__ =='__main__':
+    print(shoot4)
---
+++ slicer.py
@@ -0,0 +1,26 @@
+if __name__ =='__main__':
+    my_setting = 'y3tds23o12345dujderpvpavpss5432fjs24fsswe098765d'
+    corrected_string = my_setting.replace('3tds23', '')
+    corrected_string = corrected_string.replace('12345d', '')
+    corrected_string = corrected_string.replace('jderpv', '')
+    corrected_string = corrected_string.replace('vpss5432fjs24f', '')
+    corrected_string = corrected_string.replace('w', '')
+    corrected_string = corrected_string.replace('098765', '')
+print(corrected_string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
